0ccb4fdf2fda3066438d472758f9c812249311c6
Shijia Zhang <shijiazhang@microsoft.com>
2022-09-26 19:32:45 +0800

    More rules

--- src/NugetNinja.Analyzer/Models/PackageUniverse.cs
+++ src/NugetNinja.Analyzer/Models/PackageUniverse.cs
@@ -5,46 +5,18 @@ using Microsoft.NugetNinja.Core;
 
 namespace NugetNinja.Analyzer.Models;
 
-public class PackageUniverse
+public class PackageUniverse : PackageDictionary<PackageDescription>
 {
-    private readonly Dictionary<string, SortedDictionary<NugetVersion, PackageDescription>> _dict;
-
     public PackageUniverse()
     {
-        _dict = new Dictionary<string, SortedDictionary<NugetVersion, PackageDescription>>();
     }
 
-    public PackageUniverse(IEnumerable<PackageDescription> packages)
+    public PackageUniverse(IEnumerable<PackageDescription> packages) 
+        : base(packages.Select(p => (p.Name, p.Version, p)))
     {
-        _dict = packages
-            .GroupBy(p => p.Name, p => p)
-            .ToDictionary(
-                g => g.Key,
-                g => new SortedDictionary<NugetVersion, PackageDescription>(
-                    g.ToDictionary(p => p.Version, p => p)));
     }
 
-    public PackageDescription? GetPackageDescription(string name, NugetVersion version)
-    {
-        try
-        {
-            return _dict[name][version];
-        }
-        catch
-        {
-            return null;
-        }
-    }
+    public PackageDescription[] GetAllPackageDescriptions() => AllValues.ToArray();
 
-    public List<NugetVersion>? GetPackageVersions(string name)
-    {
-        _dict.TryGetValue(name, out var descriptions);
-        return descriptions?.Select(x => x.Value.Version).ToList();
-    }
-
-    public NugetVersion? GetPackageMaxVersion(string name)
-    {
-        _dict.TryGetValue(name, out var descriptions);
-        return descriptions?.Keys.Last();
-    }
+    public PackageDescription? GetPackageDescription(string name, NugetVersion version) => Get((name, version));
 }
